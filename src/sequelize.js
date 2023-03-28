68f6b09fcbff298150e0a99620a80536e711b582
Julia <julia.maliutsina@gmail.com>
2023-03-28 21:35:35 +0300

    port

--- src/sequelize.js
+++ src/sequelize.js
@@ -3,4 +3,5 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize('railway', 'postgres', '1sm1RuPDb4af2eOcFK8B', {
   host: 'containers-us-west-49.railway.app',
   dialect: 'postgres',
+  port: process.env.DB_PORT,
 });
