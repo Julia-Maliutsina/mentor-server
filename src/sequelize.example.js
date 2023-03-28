2cede7e27394942b09e38286362cefca6f97383d
Julia <julia.maliutsina@gmail.com>
2023-03-28 19:04:33 +0300

    configure railway

--- src/sequelize.example.js
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize('tmm_dev_db', 'user', 'pass', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
--- src/sequelize.js
+++ src/sequelize.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('tmm_dev', 'dev', 'root', {
-  host: 'localhost',
+export const sequelize = new Sequelize('railway', 'postgres', '1sm1RuPDb4af2eOcFK8B', {
+  host: 'containers-us-west-49.railway.app',
   dialect: 'postgres',
 });
